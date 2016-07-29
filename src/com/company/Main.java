45c25d2221bd220c0380e851d88de198048aada9
DY <978548481@qq.com>
2016-07-29 15:21:28 +0800

    ingkee登陆获取完善

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -98,21 +98,30 @@ public class Main {
 //        String code = scanner.next();
 
 //        System.out.println(loginjson);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()){
+            System.out.println("请输入本次要获取的条数（ps:不大于10）,最好给些接收时间：");
+            int count = scanner.nextInt();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String[] nums = GetPhones.getNumbers(count);
+                    if (nums.length!=count){
+                        System.out.println("获取不到资源，请手动释放手机号资源");
+                    }else {
+                        for (String num :
+                                nums) {
+                            System.out.println(num);
+                            GetInfoThread getInfoThread = new GetInfoThread(num);
+                            getInfoThread.run();
 
-        int count = Integer.parseInt(args[0]);
-        String[] nums = GetPhones.getNumbers(count);
-        if (nums.length!=count){
-            System.out.println("获取不到资源，请手动释放手机号资源");
-        }else {
-//            System.out.println(Arrays.toString(nums));
-            for (String num :
-                    nums) {
-                System.out.println(num);
-                GetInfoThread getInfoThread = new GetInfoThread(num);
-                getInfoThread.run();
+                        }
+                    }
+                }
+            }).start();
 
-            }
         }
+
 
 
 
